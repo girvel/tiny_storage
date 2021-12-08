9f7187b4214480a04bd8c847b87db9b567377c03
girvel <widauka@ya.ru>
2021-12-08 08:04:02 +0500

    test(integration): Entity.push

--- integration_tests/test_tiny_storage.py
+++ integration_tests/test_tiny_storage.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+import yaml
+
 from tiny_storage import Storage
 import unittest
 
@@ -15,3 +17,11 @@ class TinyStorageCase(unittest.TestCase):
         os.system(r'echo "something: 1" > ~/.test.yaml')
         self.assertEqual(self.storage('something').pull(), 1)
         self.assertEqual(self.storage('another_thing').pull(), None)
+
+    def test_push(self):
+        os.system(r'echo "something: 1" > ~/.test.yaml')
+        self.assertEqual(self.storage('something').push(), 1)
+        self.assertEqual(self.storage('another_thing').push(), True)
+
+        with open(os.getenv('HOME') / Path('.test.yaml')) as f:
+            self.assertEqual(dict(something=1, another_thing=True), yaml.safe_load(f))
