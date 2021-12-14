adac5019b30d05dc07a3803bd77cbc2ea3d42fb7
girvel <widauka@ya.ru>
2021-12-15 00:48:35 +0500

    test: change integration_tests launch mechanism
    They should be run as if tiny_storage is installed as a package
    from pip.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="tiny_storage",
-    version='1.0.23',
+    version='1.0.26',
     author="Nikita Girvel Dobrynin",
     author_email="widauka@ya.ru",
     description=
