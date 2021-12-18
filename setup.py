240dc1d95cebe323fd4ad5ef01697123287fa2be
girvel <widauka@ya.ru>
2021-12-18 20:12:07 +0500

    fix(flow): fix incorrect launch of integration tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="tiny_storage",
-    version='1.0.27',
+    version='1.0.28',
     author="Nikita Girvel Dobrynin",
     author_email="widauka@ya.ru",
     description=
