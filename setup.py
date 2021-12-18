86a623fdc270d5e002c662ed6764bacdf895bf72
girvel <widauka@ya.ru>
2021-12-18 20:09:44 +0500

    feat(flow): add coverage output

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="tiny_storage",
-    version='1.0.26',
+    version='1.0.27',
     author="Nikita Girvel Dobrynin",
     author_email="widauka@ya.ru",
     description=
--- tiny_storage/__init__.py
+++ tiny_storage/__init__.py
@@ -1,4 +1,4 @@
 from .core import Unit, Type
 
 __all__ = [e.__name__ for e in (Unit, Type)]
-__version__ = '1.0.26'
+__version__ = '1.0.27'
